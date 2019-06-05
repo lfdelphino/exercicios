bc3af4493726c52e28c9b4b114811da401cf7f24
Luiz Delphino <lf.delphino@live.com>
2019-06-05 20:23:57 -0300

    Encurtamento do código

--- 001 - Jogo Detetive/resolucao.py
+++ 001 - Jogo Detetive/resolucao.py
@@ -1,28 +1,17 @@
 # -*- coding: utf-8 -*-
 
-jogando = True
 perguntas = ["Telefonou para a vítima?", "Esteve no local do crime?", "Mora perto da vítima?", "Devia para a vítima?",
              "Já trabalhou com a vítima?"]
+julgamento = ["Inocente", "Inocente", "Suspeito(a)", "Cúmplice", "Cúmplice", "Assassino"]
 
 
-def julgamento(culpado):
-    if culpado == 2:
-        return "Suspeito(a)"
-    elif culpado == 3 or culpado == 4:
-        return "Cúmplice"
-    elif culpado == 5:
-        return "Assassino"
-    else:
-        return "Inocente"
-
-
-while jogando:
+while perguntas:
     culpado = 0
     for pergunta in perguntas:
         resposta = input(' '.join([pergunta, "(Y/n)"]))
         if "y" in resposta.lower():
             culpado += 1
 
-    fim = input("Você é {}!\nDeseja recomeçar? (Y/n)".format(julgamento(culpado)))
+    fim = input("Você é {}!\nDeseja recomeçar? (Y/n)".format(julgamento[culpado]))
     if "n" in fim.lower():
-        jogando = False
+        perguntas = False
