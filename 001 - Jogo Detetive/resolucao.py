edc2fce749490689a36bdcbbd64d02edd0e3d560
Luiz Delphino <lf.delphino@live.com>
2022-11-12 23:12:28 -0500

    Update resolucao.py

--- 001 - Jogo Detetive/resolucao.py
+++ 001 - Jogo Detetive/resolucao.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 perguntas = ["Telefonou para a vítima?", "Esteve no local do crime?", "Mora perto da vítima?", "Devia para a vítima?", "Já trabalhou com a vítima?"]
 julgamento = ["Inocente", "Inocente", "Suspeito(a)", "Cúmplice", "Cúmplice", "Assassino"]
 
@@ -7,8 +6,6 @@ while perguntas:
 	culpado = 0
 	for pergunta in perguntas:
 		resposta = str(input(' '.join([pergunta, "(S/n) "])))
-		if "s" in resposta.lower():
-			culpado += 1
-
+		culpado += 1 if "s" in resposta.lower() else culpado
 	fim = str(input("Você é {}!\nDeseja recomeçar? (Y/n)".format(julgamento[culpado])))
 	perguntas = False if "n" in fim.lower() else perguntas
